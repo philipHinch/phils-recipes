c3aeef230720bdb9ea3c3144eb5203d4aa8a9759
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-11-29 10:11:04 +0200

    fixed svg style & ajusted modal padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
 
         <div>
-            <p className="copyright" style={{ color: 'black', width: '100%', textAlign: 'center', padding: '20px', marginTop: '20px' }}>&copy;2021 - <a href="https://github.com/philipHinch" target="_blank" style={{ color: 'black' }}>https://github.com/philipHinch</a></p>
+            <p className="copyright" style={{ color: 'black', width: '100%', textAlign: 'center', padding: '20px', marginTop: '20px', opacity: 0.8 }}>&copy;2021 - <a href="https://github.com/philipHinch" target="_blank" style={{ color: 'black' }}>https://github.com/philipHinch</a></p>
         </div>
 
     );
